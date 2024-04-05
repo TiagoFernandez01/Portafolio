b2643c2e46d07464e55cbf299b505f85c55f8192
TiagoFernandez01 <tiagofernandezz01@hotmail.com>
2024-04-05 18:32:36 -0300

    test 4

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import image from '@rollup/plugin-image'
 
 
 export default defineConfig({
   plugins: [
     react(),
+    image()
   ],
   build: {
     rollupOptions: {
