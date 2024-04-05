c3eed1846a5d2c573253f0926d8bc2cdad477995
TiagoFernandez01 <tiagofernandezz01@hotmail.com>
2024-04-05 17:42:13 -0300

    re-configuration vite-config.js

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import vueImages from 'vite-plugin-vue-images'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),
-  vueImages()],
-  build:{
+  plugins: [
+    react(),
+    vueImages()
+  ],
+  build: {
     rollupOptions: {
       external: ['react-icons/Bs']
     }
