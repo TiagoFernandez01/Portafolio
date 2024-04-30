f929109b366542088a4adb5e93741cc2b8d93a89
TiagoFernandez01 <tiagofernandezz01@hotmail.com>
2024-04-30 12:11:57 -0300

    imgs fixed routes

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Chikara from '../assets/imgs/Chikara.png'
 import Weather from '../assets/imgs/Weather.png'
-import Dogs from '../assets/imgs/Dogs.png'
+import Dogs from '../assets/imgs/dogs.png'
 //import Cotizador from '../assets/imgs/cotizador.png'
 import Plate from '../assets/imgs/plate.png'
 
